d3976a6a583bf37f1d11fa4955f1774b854fb68c
thissurya123 <thissurya31@gmail.com>
2024-06-02 17:58:10 +0700

    add 2 laning pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="font-bold text-red-500 text-5xl">Surya ahmad Afandi</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App bg-indigo-700 min-h-screen">
+      <header className="bg-indigo-900 h-16 text-white flex items-center p-5">
+        <p>Uya Topup</p>
       </header>
+
+      <section className="md:m-5 bg-indigo-500 min-h-screen p-5">
+        {/* <Games /> */}
+        <Menu />
+      </section>
+
+
+
+      <footer className="bg-indigo-900 h-16">
+
+      </footer>
     </div>
   );
+}
+function Menu() {
+  return (
+    <>
+      <div className="flex justify-center">
+        <input className="m-1 p-2" type="text" placeholder="Masukkan ID" />
+        <input className="m-1 p-2" type="text" placeholder="Region(xxxxxxx)"></input>
+      </div>
+
+      <div className="bg-indigo-700 mt-5 p-5">
+        <div className="flex p-5 gap-5">
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+        </div>
+        <div className="flex p-5 gap-5">
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Games() {
+  return (
+    <>
+      <div className="bg-indigo-300 h-96">
+
+      </div>
+
+      <div className="bg-indigo-300 min-h-screen mt-5">
+        <div className="flex p-5 gap-5">
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+        </div>
+        <div className="flex p-5 gap-5">
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+        </div>
+        <div className="flex p-5 gap-5">
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+          <div className="bg-indigo-500 md:flex-grow h-64"></div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
